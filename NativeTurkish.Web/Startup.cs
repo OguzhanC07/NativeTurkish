fb5308ba7f11afd56596be513afbb374949f5320
Oğuzhan Can <59476485+OguzhanC07@users.noreply.github.com>
2021-01-17 13:09:44 +0300

    First
    First

--- NativeTurkish.Web/Startup.cs
+++ NativeTurkish.Web/Startup.cs
@@ -1,12 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NativeTurkish.Web
 {
@@ -16,6 +16,9 @@ namespace NativeTurkish.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +30,13 @@ namespace NativeTurkish.Web
             }
 
             app.UseRouting();
+            app.UseSession();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: default, pattern: "{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute(name: "areas", pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
